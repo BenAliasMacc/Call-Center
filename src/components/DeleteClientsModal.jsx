614965ce330fc9e64e18229a0c57680e69d319e9
BenAliasMacc <80431649+BenAliasMacc@users.noreply.github.com>
2022-07-10 09:59:20 +0200

    delete-modal

--- src/components/DeleteClientsModal.jsx
+++ src/components/DeleteClientsModal.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import axios from '../api/axios';
 import useAuth from '../hooks/useAuth';
+import bin from '../assets/icons/bin-modal.svg'
 
 const DeleteClientsModal = ({ clientId, refresh, setRefresh }) => {
 
@@ -56,9 +57,14 @@ const DeleteClientsModal = ({ clientId, refresh, setRefresh }) => {
 
         <div className='delete-clients-modal'>
             <div className='delete-clients-modal--container'>
-                <p>Souhaitez-vous confirmer la suppression</p>
-                <button onClick={handleCloseModal}>X</button>
-                <button onClick={handleDeleteClient}>Confirmer</button>
+                <div className='delete-clients-modal__text'>
+                    <h2><img src={bin} alt="suppression" /> Confirmer la suppression ?</h2>
+                    <p>Attention si vous cliquez sur confirmer la suppression de la fiche client sera définitive</p>
+                </div>
+                <div className='delete-clients-modal__buttons'>
+                    <button className='delete-clients-modal__buttons__cancel' onClick={handleCloseModal}>Annuler</button>
+                    <button className='delete-clients-modal__buttons__confirm' onClick={handleDeleteClient}>Confirmer</button>
+                </div>
             </div>
         </div>
     )
--- src/components/DisplayClientsData.jsx
+++ src/components/DisplayClientsData.jsx
@@ -1,311 +1,65 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "../api/axios";
-import { AiOutlineMessage } from "react-icons/ai";
-import { FaInternetExplorer } from 'react-icons/fa';
-import Loader from '../components/Loader';
 import useAuth from "../hooks/useAuth";
-import '../styles/styles.css';
-import DeleteButton from "./DeleteButton";
-import EditButton from "./EditButton";
-import DeleteClientsModal from "./DeleteClientsModal";
-import NotesEtConsignes from "./NotesEtConsignes";
 
-const DisplayClientsData = ({ client, clientId, token, booleen, setRefresh, refresh }) => {
-  
-  const { deleteClientsModal, setEditClientsModal, auth, setAuth } = useAuth();
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  
-  const [editMode, setEditMode] = useState(booleen);
-  const [isModal] = useState(booleen);
+const DeleteClientsModal = ({ clientId, refresh, setRefresh }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const { setDeleteClientsModal } = useAuth();
+  const token = localStorage.getItem("token");
+  const userId = "62b08ba33f8191dd23368c83";
   const [isLoading, setIsLoading] = useState(false);
 
-  const {
-    nom,
-    prenom,
-    id,
-    societe,
-    activite,
-    adresse,
-    mail,
-    telephone,
-    site,
-    crm,
-    consignes,
-    notes,
-    langue
-  } = client !== undefined && client;
-
-  const displayPrenom = !editMode ? (
-    prenom
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        style={{marginBottom: "5px"}}
-        type="text"
-        id="prenom"
-        defaultValue={prenom}
-        {...register("prenom")}
-      />
-    </>
-  );
-  const displayNom = !editMode ? (
-    nom
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        type="text"
-        id="nom"
-        defaultValue={nom}
-        {...register("nom")}
-      />
-    </>
-  );  
-  const displayId = !editMode ? (
-    id
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        type="number"
-        id="id"
-        defaultValue={id}
-        {...register("id")}
-      />
-    </>
-  );
-  const displaySociete = !editMode ? (
-    societe
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        type="text"
-        id="societe"
-        defaultValue={societe}
-        {...register("societe")}
-      />
-    </>
-  );
-  const displayLangue = !editMode ? (
-    langue
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        type="text"
-        id="langue"
-        defaultValue={langue}
-        {...register("langue")}
-      />
-    </>
-  );
-  const displayActivite = !editMode ? (
-    activite
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        type="text"
-        id="activite"
-        defaultValue={activite}
-        {...register("activite")}
-      />
-    </>
-  );
-  const displayTelephone = !editMode ? (
-    telephone
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        type="text"
-        id="telephone"
-        defaultValue={telephone}
-        {...register("telephone")}
-      />
-    </>
-  );
-  const displaySite = !editMode ? (
-    site
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        type="text"
-        id="site"
-        defaultValue={site}
-        {...register("site")}
-      />
-    </>
-  );
-  const displayMail = !editMode ? (
-    mail
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        type="text"
-        id="mail"
-        defaultValue={mail}
-        {...register("mail")}
-      />
-    </>
-  );
-  const displayCrm = !editMode ? (
-    crm
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        type="text"
-        id="crm"
-        defaultValue={crm}
-        {...register("crm")}
-      />
-    </>
-  );
-  const displayAdresse = !editMode ? (
-    adresse
-  ) : (
-    <>
-      <input
-        className="headerClient__input"
-        type="text"
-        id="adresse"
-        defaultValue={adresse}
-        {...register("adresse")}
-      />
-    </>
-  );
-
-  const handleCancelButton = (e) => {
-    e.preventDefault();    
-    setEditMode(!editMode);    
-  };
-
-  const handleCloseModal = () => {
-    setEditClientsModal(false);
-    setAuth({...auth, clientId: undefined})
-  }
-
-  const onSubmit = data => {
-
+  const handleDeleteClient = () => {
     setIsLoading(true);
 
-    const editClientData = async () => {
-
-      console.log(data);
-
+    const deleteClients = async () => {
       try {
-        const response = await axios.put(
-          `/clients/modifyClient/${clientId}`,
-          JSON.stringify(data),
+        const response = await axios.delete(
+          `/clients/deleteClient/${clientId}`,
           {
             headers: {
               "Content-Type": "application/json",
               Authorization: `Bearer ${token}`,
-              Accept: "application/json, text/plain,"
+              Accept: "application/json, text/plain"
+            },
+            data: {
+              userId: userId
             }
             // withCredentials: true
           }
         );
-        setEditClientsModal(false);        
         setRefresh(!refresh);
-        setEditMode(false);      
         setIsLoading(false);
+        setDeleteClientsModal(false);
+        navigate(from, { replace: true });
+        // window.location.reload();
       } catch (err) {
         console.log();
       }
     };
 
-    editClientData();
+    deleteClients();
+  };
+
+  const handleCloseModal = (e) => {
+    e.preventDefault();
+    setDeleteClientsModal(false);
   };
 
   return (
-    <section className="display-clients-data">        
-        {client !== undefined && <div className="globalContainer width100" onSubmit={handleSubmit(onSubmit)}>           
-          
-            <div className="flex containerIcons width100" >
-                <div className="flex containerIcon" >
-                  <a href={crm} rel="noreferrer" target="_blank">
-                    <AiOutlineMessage className="icon" />
-                  </a>
-                </div>
-                <div className="flex containerIcon" >
-                  <a href={site} rel="noreferrer" target="_blank">
-                    <FaInternetExplorer className="icon" />
-                  </a>
-                </div>
-            </div>
-
-            <header className='headerClient'>
-              <form>
-                {isModal ? 
-                  <button className="headerClient__close-button" onClick={handleCloseModal} style={{fontSize: "1.5rem"}}>X</button> 
-                  :
-                  <div className="display-clients-data__buttons-top">
-                    <EditButton editMode={editMode} setEditMode={setEditMode} />
-                    <DeleteButton clientId={clientId} />
-                  </div>    
-                }
-                <div className="headerClient__input-container" style={{}}>
-                  <div className="flex headerSide" >
-                      <ul className="width50">
-                          <li><b>Id :</b> {displayId}</li>
-                          <li className="mt5"><b>Nom complet :</b> {displayPrenom} {displayNom}</li>
-                          <li className="mt5"><b>Adresse : </b>{displayAdresse}</li>
-                      </ul>                  
-                      <ul>
-                          <li><b>Téléphone:</b> {displayTelephone}</li>
-                          <li className="mt5"><b>@ : </b>{displayMail}</li>
-                      </ul>
-                  </div>
-                  <div className="flex headerSide">
-                      <ul className="width50">
-                          <li><b>Société :</b> {displaySociete}</li>
-                          <li className="mt5"><b>Site : </b>{displaySite}</li>
-                      </ul>
-                      <ul>
-                          <li><b>Activité :</b> {displayActivite}</li>
-                          <li className="mt5"><b>Langue : </b>{displayLangue}</li>
-                      </ul>
-                  </div>
-                </div>
-
-                {editMode && (
-                  <div className="clients-card__buttons">
-                    {!isModal &&
-                      <button onClick={handleCancelButton} className="clients-card__buttons__cancel">
-                        Annuler
-                      </button>             
-                    }
-                    
-                    <button className="clients-card__buttons__submit">
-                      Valider
-                    </button>
-                  </div>
-                )}
-              </form>
-            </header>
-
-            {!isModal && <NotesEtConsignes clientId={clientId} token={token} client={client} />}    
-
-        </div>}
-
-        {deleteClientsModal === true && 
-          <DeleteClientsModal clientId={clientId} />
-        }
-
-        { isLoading && 
-          <div className='containerLoader'>
-            <Loader />
-          </div>
-        }
-    </section>
+    <div className="delete-clients-modal">
+      <div className="delete-clients-modal--container">
+        <h2>Confirmez-vous la suppression ?</h2>
+        <p>Attention toutes suppressions d'une fiche client est définitive</p>
+        <button onClick={handleCloseModal}>Annuler</button>
+        <button onClick={handleDeleteClient}>Confirmer</button>
+      </div>
+    </div>
   );
 };
 
-export default DisplayClientsData;
+export default DeleteClientsModal;
