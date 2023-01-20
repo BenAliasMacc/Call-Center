bf74e5bb069bbc5ac1431a1229bb9f30ba407d15
BenAliasMacc <80431649+BenAliasMacc@users.noreply.github.com>
2023-01-20 03:28:52 +0100

    logs

--- src/components/HistoricMessageModal.jsx
+++ src/components/HistoricMessageModal.jsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from "../api/axios";
+
 const HistoricMessageModal = () => {
+
+    const [historic, setHistoric] = useState([]);
+
+    useEffect(() => {
+        const getHistoric = async () => {
+            try {
+            const response = await axios.get(`http://localhost:80/api/logs/00001`, {
+                headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+                Accept: "application/json, text/plain,"
+                }
+                // withCredentials: true
+            });        
+
+            console.log(response)
+                
+            if (response.data.success === -1)  {
+                localStorage.clear();
+                navigate('/login', {state: { from: location }, replace: true });
+            }
+            setHistoric(response.data);
+            setIsLoading(false);
+            } catch (err) {
+            if (err.response?.status === 404) {
+                navigate('*', {state: { from: location }, replace: true });
+            }
+            }
+        };
+
+        console.log(historic);
+    
+        getHistoric();
+    }, []);
+
     return (
         <div className="HistoricMessageModal">
-            <h4>Historique</h4>
+            {historic?.map(log => <div>{log}</div>)}
         </div>
     );
 };
